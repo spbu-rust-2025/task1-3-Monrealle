8ef779248b6c544fb8551290f07ac167859a2248
Stanislav Klimovich <stasklimovish@gmail.com>
2025-10-27 02:52:56 +0300

    Implemented a check for the presence of a file

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,14 @@
-fn main() {
+use std::fs;
+use std::io;
 
+fn main() {
+    let mut path = String::new();
+
+    io::stdin().read_line(&mut path).unwrap();
+    let path = path.trim();
+
+    match fs::read_to_string(path) {
+        Ok(_) => println!("success"),
+        Err(_) => println!("failure"),
+    }
 }
